419dd4ab8a75aaa3a2989d224422686a7b81670a
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-09-23 16:41:14 -0300

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       "@babel/preset-env",
+//       "@babel/preset-react",
+//       "@babel/preset-typescript",
+//     ],
+//     plugins: [
+//       [
+//         "module-resolver",
+//         {
+//           root: ["."],
+//           extensions: [".ts", ".tsx", ".js", ".json"],
+//           alias: {
+//             "@assets": "./src/assets",
+//             "@clients": "./src/clients",
+//             "@services": "./src/services",
+//           },
+//         },
+//       ],
+//     ],
+//   };
+// };
